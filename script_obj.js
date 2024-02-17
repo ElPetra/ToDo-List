cf39c3255cb90e9028da91ce09ae7c69315932a1
Elpetra <k158vx@yandex.ru>
2024-02-17 21:26:19 +0300

    project is done with obj

--- script_obj.js
+++ script_obj.js
@@ -7,16 +7,16 @@ let btnsNode = document.querySelector(".btns");
 let LS = window.localStorage;
 let arr = [];
 
-let createContent = (text) => {
+let createContent = (text, check) => {
   let firstUpperLetter = (str) => str.split("")[0].toUpperCase() + str.slice(1);
   let content = document.createElement("div");
   content.classList.add("content");
   content.innerHTML = `
     <div class="left">
       <label class="check option">
-        <input type="checkbox" class="checkbox" />
+        <input type="checkbox" class="checkbox" ${check === "text_checked" ? "checked" : ""}/>
         <span class="check__box"></span>
-        <p class="text">
+        <p class= "text ${check}">
           ${firstUpperLetter(text)}
         </p>
       </label>
@@ -34,18 +34,12 @@ let changeLS = (text) => {
   LS.setItem("toDo", JSON.stringify(arr));
 };
 
-if (LS.x) {
-  arr = JSON.parse(LS.getItem("toDo"));
-  for (let el of arr) {
-    createContent(el.task);
-  }
-}
-
-let deleteItem = (item) => {
-  let value = item.closest(".content").querySelector(".text").innerText;
-  arr.splice(arr.indexOf(value), 1);
-  LS.setItem("toDo", arr);
-  item.closest(".content").remove();
+let deleteItem = (value) => {
+  let textItem = value.closest(".content").querySelector(".text").innerText;
+  arr.splice(
+    arr.findIndex((el) => el.task === textItem),1);
+  LS.setItem("toDo", JSON.stringify(arr));
+  value.closest(".content").remove();
   if (arr.length === 0) {
     btnsNode.classList.add("d-none");
   }
@@ -56,6 +50,8 @@ btnDelFinished.addEventListener("click", function () {
   for (let el of AllTextCheckeds) {
     deleteItem(el);
   }
+  arr = arr.filter(el => el.check === false)
+  LS.setItem("toDo", JSON.stringify(arr));
 });
 
 container.addEventListener("click", function (event) {
@@ -64,10 +60,13 @@ container.addEventListener("click", function (event) {
   }
 
   if (event.target.classList == "checkbox") {
-    event.target
-      .closest(".content")
-      .querySelector(".text")
-      .classList.toggle("text_checked");
+    let textItem = event.target.closest(".content").querySelector(".text");
+    textItem.classList.toggle("text_checked");
+    let checkbox = document.querySelector(".checkbox");
+    checkbox.toggleAttribute("checked");
+    let variable = arr.findIndex((el) => el.task === textItem.innerText);
+    arr[variable].check = arr[variable].check ? false : "text_checked";
+    LS.setItem("toDo", JSON.stringify(arr));
   }
 });
 
@@ -79,6 +78,14 @@ formNode.addEventListener("submit", function (event) {
     createContent(inputNode.value);
   }
 });
+
+if (LS.toDo) {
+  btnsNode.classList.remove("d-none");
+  arr = JSON.parse(LS.getItem("toDo"));
+  for (let el of arr) {
+    createContent(el.task, el.check);
+  }
+}
 
 btnDelAll.addEventListener("click", function () {
   container.innerHTML = "";
